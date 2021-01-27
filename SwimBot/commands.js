cff1bb3c5d4d19a53f0c49d82b35899c8ec3d726
NamanPrakash <74247845+NamanPrakash@users.noreply.github.com>
2021-01-27 21:06:59 +0530

    Fixed Bugs

--- SwimBot/commands.js
+++ SwimBot/commands.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
+prefix = process.env.PREFIX;
 module.exports = async function Command(msg)
 {
     if (msg.channel.id == '786186761754640424')
     {
         console.log(msg.content);
-        if(!msg.content.startsWith(process.env.PREFIX) || msg.author.bot) return;
-        args = msg.content.slice(process.env.PREFIX.length).trim().split(/ +/);
+        if(!msg.content.startsWith(prefix) || msg.author.bot) return;
+        args = msg.content.slice(prefix.length).trim().split(/ +/);
         command = args.shift().toLowerCase();
 
         if(command == "hello"){
